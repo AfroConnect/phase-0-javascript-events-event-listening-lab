ee46cfb74f79038d6d52dea4ddf49f74b68e036e
AfroConnect <mensahmedia23@gmail.com>
2023-05-07 03:51:17 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('Your Funny!I like the way you teach');
 }
+
+input.addEventListener('click', clickAlert);
+
+
+
+
+
+
+
+
+function addingEventListener() {
+    let input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      console.log('Button clicked');
+    });
+  }
+
+
+
+
+
